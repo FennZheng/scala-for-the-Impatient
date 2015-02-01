acfb72e7a38154c1dd2352bf7144904199072e6c
Vernon Zheng <kevonzheng@gmail.com>
2015-02-01 15:06:34 +0800

    fixed conflicts

--- src/Chapter05/exercise06.scala
+++ src/Chapter05/exercise06.scala
@@ -1,6 +1,8 @@
 /**
  * 在5.2节的Person类中提供一个主构造器,将负年龄转换为0
  */
-class Person(var age:Int){
-  age = if(age<0) 0 else age
+package Q5_6{
+  class Person(var age:Int){
+    age = if(age<0) 0 else age
+  }
 }
