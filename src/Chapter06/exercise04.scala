dc25f77807206226af8b269e7c1df5361a442fcd
Vernon Zheng <kevonzheng@gmail.com>
2015-02-01 09:14:26 +0800

    update

--- src/Chapter06/exercise04.scala
+++ src/Chapter06/exercise04.scala
@@ -1,8 +1,12 @@
 /**
  * 定义一个 Point 类和一个伴生对象,使得我们可以不用 new 而直接用 Point(3,4)来构造 Point 实例 apply 方法的使用
  */
+package Q64 {
+
 class Point(x: Int = 0, y: Int = 0) extends java.awt.Point(x, y)
 
 object Point {
   def apply(x: Int = 0, y: Int = 0) = new Point(x, y)
+}
+
 }
