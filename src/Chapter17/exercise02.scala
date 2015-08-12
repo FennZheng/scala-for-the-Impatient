424f130999646d9a3d067b05e2f1f386c614278f
Vern Zheng <kevonzheng@gmail.com>
2015-08-12 16:29:59 +0800

    Update exercise02.scala

--- src/Chapter17/exercise02.scala
+++ src/Chapter17/exercise02.scala
@@ -1,6 +1,11 @@
 /**
  * 定义一个可变类Pair[T]，带一个swap方法，交换对偶中组件的位置。
  */
-class Pair[T](val s:T,val t:T){
-  def swap() = new Pair(t,s)
+class Pair[T] (var s: T, var t: T) {
+  def swap() = {
+    val temp = s
+    this.s = t
+    this.t = temp
+  }
+  override def toString = s"s: $s, t: $t"
 }
